f9235a03dcf34ee964adc30a52f98c59974cfab7
jatindhobi <131326165+jatindhobi@users.noreply.github.com>
2024-07-10 21:48:14 +0530

    user stay logged in

--- Screens/DashboardScreen/DashboardScreen.tsx
+++ Screens/DashboardScreen/DashboardScreen.tsx
@@ -1,24 +1,12 @@
 import { Appearance, SafeAreaView, Text } from "react-native";
 import GlobalStyle from "../../Assets/GlobalStyles/GlobalStyle";
-import { Routes } from "../../Navigation/Routes";
-import LoginSignUpButton from "../../Components/LoginSignUpButton/LoginSignUpButton.tsx";
-import {firebase} from "@react-native-firebase/auth";
-const DashboardScreen = ({navigation}: { navigation: any }) => {
+const DashboardScreen = () => {
     const colorSchema = Appearance.getColorScheme();
-
     return (
         <SafeAreaView style={[GlobalStyle.globalBackgroundFlex, GlobalStyle.globalAppBackground]}>
             <Text style={{color: '#FFF'}}>
                 Top 5 used apps
             </Text>
-            <LoginSignUpButton text={'SignOut'} onPress={()=>{
-                firebase.auth().signOut().then(() => {
-                    navigation.navigate(Routes.LoginScreen);
-                }).catch(error => {
-                    console.log(error)
-                });
-            }} buttonColor={'#1936c5'} textColor={'#fff'} isEnabled={true}
-                               buttonRadius={8} leftMargin={30} topMargin={50}/>
         </SafeAreaView>
     );
 };

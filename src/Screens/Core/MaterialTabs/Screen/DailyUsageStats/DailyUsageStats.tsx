1cab75a265c9b88b35b47b019821320cdf7b08d7
Pranav Sinha <pranavsinha922@gmail.com>
2024-10-09 10:01:35 +0530

    Added the chart kit package and removed some of the unused packages.

--- src/Screens/Core/MaterialTabs/Screen/DailyUsageStats/DailyUsageStats.tsx
+++ src/Screens/Core/MaterialTabs/Screen/DailyUsageStats/DailyUsageStats.tsx
@@ -1,14 +1,35 @@
-import {SafeAreaView, Text, useColorScheme} from 'react-native';
+import {Dimensions, SafeAreaView, Text, useColorScheme, View} from 'react-native';
+import {
+	LineChart,
+} from "react-native-chart-kit";
 
 const DailyUsageStats = () => {
 	const colorSchema = useColorScheme();
+	const screenWidth = Dimensions.get("window").width;
+	const data = {
+		labels: ["January", "February", "March", "April", "May", "June"],
+		datasets: [
+			{
+				data: [20, 45, 28, 80, 99, 43],
+				color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+				strokeWidth: 2 // optional
+			}
+		],
+		legend: ["Rainy Days"] // optional
+	};
 	return (
-		<SafeAreaView style={{flex: 1, backgroundColor: colorSchema === 'dark' ? "#000" : "#FFF"}}>
-			<Text>
-				Daily App Usage Graph
-			</Text>
+		<SafeAreaView style={{flex: 1, backgroundColor: colorSchema === 'dark' ? "#FFF" : "#FFF"}}>
+			<View>
+				<LineChart
+					data={data}
+					width={screenWidth}
+					height={220}
+				/>
+			</View>
 		</SafeAreaView>
 	);
 };
 
 export default DailyUsageStats;
+
+
